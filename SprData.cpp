9e25b93c70ac19074cf5d5f95e3942e09430fe93
KingClub7 <96010932+KingClub7@users.noreply.github.com>
2022-01-06 14:11:56 +0845

    Update SprData.cpp

--- SprData.cpp
+++ SprData.cpp
@@ -10,3 +10,5 @@ LOAD_TEXTURE load_texture = {
 SPRITE_LEFTTOP spr_back(&texBack00, 0, 0, 640, 480);
 SPRITE_CENTER  spr_player(&texSpr000, 128, 0, 64, 32);
 SPRITE_CENTER  spr_enemy(&texSpr000, 96, 96, 32, 32);
+
+//kankeinashi
